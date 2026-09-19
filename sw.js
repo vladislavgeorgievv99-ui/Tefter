fbf9b32916f10b790c60f462e699c467aa1b2c77
vladislavgeorgievv99-ui <vladislav.georgievv99@gmail.com>
2026-09-19 18:22:01 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+var CACHE = "ledger-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-512-maskable.png",
+  "./icons/favicon-32.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) { return cache.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      var network = fetch(event.request).then(function (res) {
+        if (res && res.status === 200 && res.type === "basic") {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (cache) { cache.put(event.request, copy); });
+        }
+        return res;
+      }).catch(function () { return cached; });
+      return cached || network;
+    })
+  );
+});
